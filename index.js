084264b83212a3643adbb3ad805defde5bd0ac83
Nithin-Goka <130104015+Nithin-Goka@users.noreply.github.com>
2023-08-09 18:27:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+var typed = new Typed('#element', {
+    strings: ['Web Developer' , 'Web Designer' ,'Programmer...'],
+    typeSpeed: 50,
+});
+
+
+
+function Validate(){
+//     var name =
+//         document.forms.ValidForm.Name.value;
+//     var email =
+//         document.forms.ValidForm.EMail.value;
+//     var phone =
+//         document.forms.ValidForm.Contact_Number.value;
+//     var message =
+//         document.forms.ValidForm.Message.value;
+//     //Javascript reGex for Email Validation.
+//     var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g; 
+//     // Javascript reGex for Phone Number validation.
+//     var regPhone=/^\d{10}$/;                       
+//     // Javascript reGex for Name validation
+//     var regName = /\d+$/g;                         
+ 
+//     if (name == "" || regName.test(name)) {
+//         window.alert("Please enter your name properly.");
+//         name.focus();
+//         return false;
+//     }
+    
+//     if (email == "" || !regEmail.test(email)) {
+//         window.alert("Please enter a valid e-mail address.");
+//         email.focus();
+//         return false;
+//     }
+    
+//     if (phone == "" || !regPhone.test(phone)) {
+//         alert("Please enter valid phone number.");
+//         phone.focus();
+//         return false;
+//     }
+
+//     if ( message== "") {
+//         window.alert("Please enter your message.");
+//         address.focus();
+//         return false;
+//     }
+ 
+//     return true;
+// } {
+//     var name =
+//         document.forms.ValidForm.Name.value;
+//     var email =
+//         document.forms.ValidForm.EMail.value;
+//     var phone =
+//         document.forms.ValidForm.Contact_Number.value;
+//     var message =
+//         document.forms.ValidForm.Message.value;
+//     //Javascript reGex for Email Validation.
+//     var regEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g;
+//     // Javascript reGex for Phone Number validation.
+//     var regPhone=/^\d{10}$/;                                       
+//     // Javascript reGex for Name validation
+//     var regName = /\d+$/g;                                   
+     
+ 
+//     if (name == "" || regName.test(name)) {
+//         window.alert("Please enter your name properly.");
+//         name.focus();
+//         return false;
+//     }
+ 
+//     if (message == "") {
+//         window.alert("Please enter your address.");
+//         address.focus();
+//         return false;
+//     }
+     
+//     if (email == "" || !regEmail.test(email)) {
+//         window.alert("Please enter a valid e-mail address.");
+//         email.focus();
+//         return false;
+//     }
+      
+//     if (phone == "" || !regPhone.test(phone)) {
+//         alert("Please enter valid phone number.");
+//         phone.focus();
+//         return false;
+//     }
+ 
+//     return true;
+// }
+alert("correct");
+}
---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+	// header('Access-Control-Allow-Origin: *');
+	
+	// $conn = new mysqli("127.0.0.1","root","");
+	
+	// if(mysqli_connect_error()){
+	// 	echo mysqli_connect_error();
+	// 	exit();
+	// }
+	// else{
+	// 	$Name = $_POST['Name'];
+	// 	$Email = $_POST['Email'];
+    //     $Contact_Number= $_['Contact_Number'];
+	// 	$Message = $_POST['Message'];
+
+		
+	// 	$sql = "INSERT INTO comments(Name,Email,Contact_Number,Message) VALUES('$Name','$Email','$Contact_Number','$Message');";
+	// 	$res = mysqli_query($conn, $sql);
+		
+	// 	if($res){
+	// 		echo "Success!";
+	// 	}
+	// 	else{
+	// 		echo "Error!";
+	// 	}
+	// 	$conn->close();
+	// }
+
+?>
